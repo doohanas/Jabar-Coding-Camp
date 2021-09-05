0e911512dfabc65b9d4b0ae40cbb3712b538e005
Doohan AS <79241754+doohanas@users.noreply.github.com>
2021-09-05 22:53:25 +0700

    Answer question number 2

--- Quiz-1/index.js
+++ Quiz-1/index.js
@@ -1,5 +1,4 @@
 // Soal 1
-
 // Judul : Function Penghitung Jumlah Kata
 
 function jumlah_kata(words) {
@@ -8,3 +7,19 @@ function jumlah_kata(words) {
   }).length;
 }
 console.log(jumlah_kata(" Halo    nama saya Muhammad Iqbal Mubarok")); //Result 6
+
+// Soal 2
+// Judul: Function Penghasil Tanggal Hari Esok
+
+
+function next_date(year, month, day) {
+  var date = new Date(year, month, day);
+
+  var nextDate = new Date(date);
+  nextDate.setDate(date.getDate());
+  nextDate.setDate(date.getMonth());
+
+  return nextDate;
+}
+
+console.log((next_date(2020, 1, 5)));
